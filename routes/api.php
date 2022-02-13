29da221bedf145e4f646fd0f2365930fcdd535f9
Bemiras <bartektojestemja12@gmail.com>
2022-02-13 22:00:17 +0100

    Auth Controller

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,25 @@ use App\Http\Controllers\ProductController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Routes for 1 Product catalog API
+//Add a new product
+Route::post('/products', [ProductController::class, 'store']);
+//Update product title and/or price
+Route::put('/products/{index}',[ProductController::class, 'update']);
+//List all of the products
+Route::get('/products', [ProductController::class, 'index']);
+
+
+/*
 Route::get('/products', [ProductController::class, 'index']) ->name('show.products');
 
 Route::get('/products/{id}', [ProductController::class, 'show']) ->name('show.product');
+/*;
