4e0144c85dfe1e0c56678d1f5c52e59c2f9479c5
Bemiras <bartektojestemja12@gmail.com>
2022-02-13 22:16:31 +0100

    Auth Controllerv2

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +23,11 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+/*
+Route::get('/products', [ProductController::class, 'index']) ->name('show.products');
+
+Route::get('/products/{id}', [ProductController::class, 'show']) ->name('show.product');
+*/
 
 //Routes for 1 Product catalog API
 //Add a new product
@@ -30,10 +36,3 @@ Route::post('/products', [ProductController::class, 'store']);
 Route::put('/products/{index}',[ProductController::class, 'update']);
 //List all of the products
 Route::get('/products', [ProductController::class, 'index']);
-
-
-/*
-Route::get('/products', [ProductController::class, 'index']) ->name('show.products');
-
-Route::get('/products/{id}', [ProductController::class, 'show']) ->name('show.product');
-/*;
